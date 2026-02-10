71fa5d38de2834e124f7fdd6ead57061a2deb773
jackieyang1727 <jiaqi.yang1@ucdconnect.ie>
2026-02-10 09:45:06 +0000

    data scraping code shared
    Getting on time bike data and hourly weather data via API connection.

---
+++ data_scraping.py
@@ -0,0 +1,58 @@
+#This file will focusing on data scraping and formating
+
+import requests
+import dbinfo
+import json
+
+def fetch_bike_data():
+    try:
+        #request data
+        response = requests.get(dbinfo.bike_url, params={"apiKey": dbinfo.bike_api_key, "contract": dbinfo.contract_city}, timeout=10)
+        #check response status
+        response.raise_for_status()
+        #save data
+        bike_data = response.json()
+
+        print("Request successful!")
+        return bike_data
+
+    except requests.exceptions.HTTPError as e:
+        print("HTTP error occurred:", e)
+    except requests.exceptions.ConnectionError:
+        print("Network connection error")
+    except requests.exceptions.Timeout:
+        print("Request timed out")
+    except requests.exceptions.RequestException as e:
+        print("Other request error:", e)
+    #if there is any error, return None
+    return None
+
+#updated_bike_data = fetch_bike_data()
+#print(updated_bike_data)
+
+def fetch_weather_data():
+    try:
+        #request data
+        response = requests.get(dbinfo.weather_url, params={"appid": dbinfo.weather_api_key, "q": dbinfo.q_city, "lang": "en", "mode": "json"}, timeout=10)
+        #check response status
+        response.raise_for_status()
+        #save data
+        weather_data = response.json()
+
+        print("Request successful!")
+        return weather_data
+
+    except requests.exceptions.HTTPError as e:
+        print("HTTP error occurred:", e)
+    except requests.exceptions.ConnectionError:
+        print("Network connection error")
+    except requests.exceptions.Timeout:
+        print("Request timed out")
+    except requests.exceptions.RequestException as e:
+        print("Other request error:", e)
+    #if there is any error, return None
+    return None
+    
+updated_weather_data = fetch_weather_data()
+with open("weather_data.txt", "w", encoding="utf-8") as f:
+    json.dump(updated_weather_data, f, indent=4, ensure_ascii=False)
---
+++ dbinfo[example].py
@@ -0,0 +1,16 @@
+#set parameter for bike data requests
+bike_api_key = "" #fill with your own api key
+bike_url = "https://api.jcdecaux.com/vls/v1/stations"
+contract_city = "dublin"
+
+#set parameter for weather data requests
+weather_api_key = "" #fill with your own api key
+weather_url = "https://pro.openweathermap.org/data/2.5/forecast/hourly"
+q_city = "Dublin"
+
+
+#set parameter for sql connection
+sqlusername = "" #fill with your own sql info
+sqlpassword = "" #fill with your own sql info
+sqlport = "3306"
+sqlurl = "127.0.0.1"
