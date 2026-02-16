2b147469a7c2f2d6aebb6781d5d6b4008e28c7af
jackieyang1727 <jiaqi.yang1@ucdconnect.ie>
2026-02-16 12:37:12 +0000

    Update data_scraping.py
    2 seperate functions for forecast and current

--- data_scraping.py
+++ data_scraping.py
@@ -30,10 +30,33 @@ def fetch_bike_data():
 #updated_bike_data = fetch_bike_data()
 #print(updated_bike_data)
 
-def fetch_weather_data():
+def fetch_weather_forecast_data():
     try:
         #request data
-        response = requests.get(dbinfo.weather_url, params={"appid": dbinfo.weather_api_key, "q": dbinfo.q_city, "lang": "en", "mode": "json"}, timeout=10)
+        response = requests.get(dbinfo.weather_url_forecast, params={"appid": dbinfo.weather_api_key, "q": dbinfo.q_city, "lang": "en", "mode": "json"}, timeout=10)
+        #check response status
+        response.raise_for_status()
+        #save data
+        weather_data = response.json()
+
+        print("Request successful!")
+        return weather_data
+
+    except requests.exceptions.HTTPError as e:
+        print("HTTP error occurred:", e)
+    except requests.exceptions.ConnectionError:
+        print("Network connection error")
+    except requests.exceptions.Timeout:
+        print("Request timed out")
+    except requests.exceptions.RequestException as e:
+        print("Other request error:", e)
+    #if there is any error, return None
+    return None
+
+def fetch_weather_current_data():
+    try:
+        #request data
+        response = requests.get(dbinfo.weather_url_current, params={"appid": dbinfo.weather_api_key, "q": dbinfo.q_city, "lang": "en", "mode": "json"}, timeout=10)
         #check response status
         response.raise_for_status()
         #save data
@@ -53,6 +76,13 @@ def fetch_weather_data():
     #if there is any error, return None
     return None
     
-updated_weather_data = fetch_weather_data()
-with open("weather_data.txt", "w", encoding="utf-8") as f:
-    json.dump(updated_weather_data, f, indent=4, ensure_ascii=False)
+updated_weather_current_data = fetch_weather_current_data()
+with open("weather_current_data.txt", "w", encoding="utf-8") as f:
+    json.dump(updated_weather_current_data, f, indent=4, ensure_ascii=False)
+
+updated_weather_forecast_data = fetch_weather_forecast_data()
+with open("weather_forecast_data.txt", "w", encoding="utf-8") as f:
+    json.dump(updated_weather_forecast_data, f, indent=4, ensure_ascii=False)
+
+#updated_weather_current_data = fetch_weather_current_data()
+#print(updated_weather_current_data, type(updated_weather_current_data))
