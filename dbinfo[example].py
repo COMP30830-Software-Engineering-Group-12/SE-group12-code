cb3757e8dc3096d3c5be14373102e43f32c90df9
jackieyang1727 <jiaqi.yang1@ucdconnect.ie>
2026-02-16 12:56:46 +0000

    Update dbinfo[example].py

--- dbinfo[example].py
+++ dbinfo[example].py
@@ -1,17 +1,17 @@
 #set parameter for bike data requests
-bike_api_key = "203abfd95de19fda4c9f5f6c3faa2bb802d4dbbc"
+bike_api_key = "" #fill with your own api key
 bike_url = "https://api.jcdecaux.com/vls/v1/stations"
 contract_city = "dublin"
 
 #set parameter for weather data requests
-weather_api_key = "57d3879c2b9569c4e574eff46823e201"
-weather_url_forecast = "https://pro.openweathermap.org/data/2.5/forecast/hourly"
+weather_api_key = "" #fill with your own api key
+weather_url = "https://pro.openweathermap.org/data/2.5/forecast/hourly"
 weather_url_current = "https://api.openweathermap.org/data/2.5/weather"
 q_city = "Dublin"
 
 
 #set parameter for sql connection
-sqlusername = "root"
-sqlpassword = "Xuexi@123"
+sqlusername = "" #fill with your own sql info
+sqlpassword = "" #fill with your own sql info
 sqlport = "3306"
 sqlurl = "127.0.0.1"
