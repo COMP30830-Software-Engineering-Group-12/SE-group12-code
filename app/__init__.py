df9a05d208e37664232d72b2ea17c2e2aa3b18fe
caoimhecoveneymckeown <caoimhe.coveneymckeown@ucdconnect.ie>
2026-02-27 13:49:40 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from .routes import main
-    app.register_blueprint(main)
+    from .api_routes import api
+    from .page_routes import pages
+
+    app.register_blueprint(api)
+    app.register_blueprint(pages)
 
     return app
